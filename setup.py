22bac967623058978505bb70d38bb643d5a73872
Michael Doronin <warrior2031@gmail.com>
2016-10-31 17:18:49 +0300

    renamed files

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author='Michael Doronin',
     author_email='warrior2031@mail.com',
     install_requires=['jsonschema', 'more_functools', 'split'],
-    py_modules=['parser'],
+    py_modules=['easyconf'],
     license='MIT',
     classifiers=(
         'Programming Language :: Python :: 3.5',
