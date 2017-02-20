9755dbd55779528584996c42a27857c2d621eaa4
Michael Doronin <warrior2031@gmail.com>
2017-02-20 14:20:16 +0300

    working setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,14 @@ setup(
     version='0.1',
     author='Michael Doronin',
     author_email='warrior2031@mail.com',
-    install_requires=['jsonschema', 'more-functools', 'split'],
+    install_requires=['jsonschema', 'more_functools', 'split>=1.0'],
     py_modules=['easyconf'],
     license='MIT',
     classifiers=(
         'Programming Language :: Python :: 3.5',
+    ),
+    dependency_links=(
+        'https://github.com/purpleP/more_functools/tarball/master#egg=more_functools-1.0',
+        'https://bitbucket.org/warrior2031/python-split/get/tip.zip#egg=split-1.0',
     ),
 )
