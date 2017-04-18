15b8c41376902550bce8877e4ab51d09d30e3ce9
Michael Doronin <warrior2031@gmail.com>
2017-04-19 00:00:02 +0300

    WIP

--- easyconf.py
+++ easyconf.py
@@ -2,107 +2,148 @@ import json
 import sys
 import types
 from argparse import ArgumentParser
-from collections import Mapping, namedtuple
-from itertools import chain
+from collections import Mapping, namedtuple, deque
+from itertools import chain, takewhile
 from functools import partial
 
 from jsonschema import validate
 from more_functools import merge
-from split import groupby
 
 
-def parse_args(schema):
-    namespace = make_argparser(schema).parse_args()
-    config = namespace_to_dict(namespace)
-    validate(config, schema)
-    import conf
-    conf.conf = config
-    return config
+class Buffered:
+    def __init__(self, sequence, size=1):
+        self.buffer = deque(maxlen=size)
+        self.sequence = iter(sequence)
+        self.from_buffer = False
+        self.ended = False
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.from_buffer:
+            try:
+                return self.buffer.popleft()
+            except IndexError:
+                if self.ended:
+                    raise StopIteration()
+                else:
+                    self.from_buffer = False
+                    return next(self)
+        try:
+            elem = next(self.sequence)
+            self.buffer.append(elem)
+            return elem
+        except StopIteration:
+            self.ended = True
+            raise
+
+def group_by_argname(args):
+    iargs = Buffered(args, 1)
+    for arg in iargs:
+        if arg.startswith('--conf.'):
+            yield arg[7:], takewhile(lambda a: not a.startswith('--conf'), iargs)
+            iargs.from_buffer = True
 
 
-def paths_from_namespace(namespace_dict):
-    return (
-        (k.split('.'), v)
-        for k, v in namespace_dict.items() if v is not None
-    )
-
-
-def namespace_to_dict(namespace):
-    namespace_dict = vars(namespace)
-    defaults = namespace_dict.pop('conf') or {}
-    conf = dict_from_paths(*paths_from_namespace(namespace_dict))['conf']
-    return merge(defaults, conf)
-
-
-def dict_from_paths(*paths):
-    def new_path(path):
-        (key, *rest), value = path
-        return rest, value
-    if len(paths) == 1 and len(paths[0][0]) == 0:
-        return paths[0][1]
+def make_dict(schema, args):
+    groups = group_by_argname(args)
     return {
-        key: dict_from_paths(*map(new_path, remaining_paths))
-        for key, remaining_paths in groupby(paths, lambda ks: ks[0][0])
+        key: make_value(key, subschema, groups)
+        for key, subschema in schema['properties'].items()
     }
 
 
-def chain_path(key, path):
-    return chain((key,), path[0]), path[1]
-
-
-def paths_from_dict(dct):
-    def make_path(k, v):
-        if isinstance(v, Mapping):
-            map(partial(chain_path, k), paths_frm_dict(v))
-        else:
-            ((key,), v)
-    return (make_path(k, v) for k, v in dct.items())
-
-
-def make_argparser(schema):
-    parser = ArgumentParser(description=schema.get('description', ''))
-    for kwargs in schema_to_kwargs(schema, name='conf'):
-        name = kwargs.pop('name')
-        parser.add_argument(name, **kwargs)
-    return parser
-
-
-def schema_to_kwargs(schema, name):
-    kwargs = {**common_kwargs(schema, name), **specific_kwargs(schema)}
-    flattened_dict_args = ()
-    if schema['type'] == 'object':
-        flattened_dict_args = chain.from_iterable(
-            schema_to_kwargs(pschema, '.'.join((name, pname)))
-            for pname, pschema in schema['properties'].items()
-        )
-    return chain((kwargs,), flattened_dict_args)
-
-
-def specific_kwargs(schema):
-    kwargs = {}
-    type = schema['type']
-    if type == 'array':
-        if len(schema['items']) > 1:
-            raise Exception(
-                'Easyconf doesn\'t support array with tuple validation.\n{}'.format(schema)
-            )
-        kwargs['type'] = jsonschema_types[schema['items'][0]['type']]
-        kwargs['nargs'] = '+'
-    elif type == 'object':
-        kwargs['type'] = json.loads
-    elif type == 'boolean':
-        kwargs['action'] = 'store_true'
-    else:
-        kwargs['type'] = jsonschema_types[type]
-    return kwargs
-
-
-def common_kwargs(schema, name):
-    result = dict(
-        name='--' + name,
-        help=schema.get('description', ''),
+def make_value(key, schema, groups):
+    groups_for_key = tuple(
+        (argname, values)
+        for argname, values if argname.startswith(key + '.') == key
     )
-    return result
+
+
+# def paths_from_namespace(namespace_dict):
+#     return (
+#         (k.split('.'), v)
+#         for k, v in namespace_dict.items() if v is not None
+#     )
+
+
+# def namespace_to_dict(namespace):
+#     namespace_dict = vars(namespace)
+#     defaults = namespace_dict.pop('conf') or {}
+#     conf = dict_from_paths(*paths_from_namespace(namespace_dict))['conf']
+#     return merge(defaults, conf)
+
+
+# def dict_from_paths(*paths):
+#     def new_path(path):
+#         (key, *rest), value = path
+#         return rest, value
+#     if len(paths) == 1 and len(paths[0][0]) == 0:
+#         return paths[0][1]
+#     return {
+#         key: dict_from_paths(*map(new_path, remaining_paths))
+#         for key, remaining_paths in groupby(paths, lambda ks: ks[0][0])
+#     }
+
+
+# def chain_path(key, path):
+#     return chain((key,), path[0]), path[1]
+
+
+# def paths_from_dict(dct):
+#     def make_path(k, v):
+#         if isinstance(v, Mapping):
+#             map(partial(chain_path, k), paths_frm_dict(v))
+#         else:
+#             ((key,), v)
+#     return (make_path(k, v) for k, v in dct.items())
+
+
+# def make_argparser(schema):
+#     parser = ArgumentParser(description=schema.get('description', ''))
+#     for kwargs in schema_to_kwargs(schema, name='conf'):
+#         name = kwargs.pop('name')
+#         parser.add_argument(name, **kwargs)
+#     return parser
+
+
+# def schema_to_kwargs(schema, name):
+#     kwargs = {**common_kwargs(schema, name), **specific_kwargs(schema)}
+#     flattened_dict_args = ()
+#     if schema['type'] == 'object':
+#         flattened_dict_args = chain.from_iterable(
+#             schema_to_kwargs(pschema, '.'.join((name, pname)))
+#             for pname, pschema in schema['properties'].items()
+#         )
+#     return chain((kwargs,), flattened_dict_args)
+
+
+# def specific_kwargs(schema):
+#     kwargs = {}
+#     type = schema['type']
+#     if type == 'array':
+#         if len(schema['items']) > 1:
+#             raise Exception(
+#                 'Easyconf doesn\'t support array with tuple validation.\n{}'.format(schema)
+#             )
+#         kwargs['type'] = jsonschema_types[schema['items'][0]['type']]
+#         kwargs['nargs'] = '+'
+#     elif type == 'object':
+#         kwargs['type'] = json.loads
+#     elif type == 'boolean':
+#         kwargs['action'] = 'store_true'
+#     else:
+#         kwargs['type'] = jsonschema_types[type]
+#     return kwargs
+
+
+# def common_kwargs(schema, name):
+#     result = dict(
+#         name='--' + name,
+#         help=schema.get('description', ''),
+#     )
+#     return result
 
 
 jsonschema_types = {
--- test_easyconf.py
+++ test_easyconf.py
@@ -1,52 +1,22 @@
 import json
+
 import pytest
-from easyconf import dict_from_paths
-from easyconf import schema_to_kwargs
-from easyconf import common_kwargs
-from frozendict import frozendict
+
+from easyconf import Buffered, group_by_argname
 
 
-def test_make_dict():
-    paths = (
-        (('some', 'arg1'), 1),
-        (('some', 'arg2'), 2),
-        (('level0',), 0),
-    )
-    expected_result = {
-        'some': {
-            'arg1': 1,
-            'arg2': 2,
-        },
-        'level0': 0,
-    }
-    assert expected_result == dict_from_paths(*paths)
+def test_buffered():
+    b = Buffered(iter(range(2)))
+    assert 0 == next(b)
+    b.from_buffer = True
+    assert 0 == next(b)
+    assert 1 == next(b)
+    b.from_buffer = True
+    assert 1 == next(b)
 
 
-def test_common_kwargs():
-    schema = {'description': 'd'}
-    expected_kwargs = {'name': '--t', 'help': 'd'}
-    assert expected_kwargs == common_kwargs(schema, name='t')
 
-
-def test_schema_to_kwargs():
-    schema = {
-        'type': 'object', 'properties': {
-            'p0': {
-                'type': 'integer',
-            },
-            'p1': {
-                'type': 'array',
-                'items': [
-                    {'type': 'integer'},
-                ],
-            }
-        }
-    }
-    expected_kwargs = {
-        frozendict(type=json.loads, help='', name='--n'),
-        frozendict(type=int, help='', name='--n.p0'),
-        frozendict(type=int, help='', name='--n.p1', nargs='+'),
-    }
-    assert expected_kwargs == set(
-        map(frozendict, schema_to_kwargs(schema, name='n'))
-    )
+def test_path_key_pairs():
+    args = ('--conf.a', 'some', 'other', '--conf.b', 'foo')
+    expected = [('a', ['some', 'other']), ('b', ['foo'])]
+    assert expected == [(argname, list(values)) for argname, values in group_by_argname(args)]
